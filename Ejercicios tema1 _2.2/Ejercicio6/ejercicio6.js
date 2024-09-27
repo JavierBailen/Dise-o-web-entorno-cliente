4364095bbd291610176cd21d20e6b9611d7c2a47
JavierBailen <javier.bailen@fernando3martos.com>
2024-09-27 20:37:15 +0200

    segunda bateria

---
+++ Ejercicios tema1 _2.2/Ejercicio6/ejercicio6.js
@@ -0,0 +1,20 @@
+//PRIMERA EXPRESION
+let x = 30 
+let y = "a" 
+y === "b" || x >= 30
+
+//La primera expresión devolverá true ya que la condición se cumple si "y" es igual a "b" o si "x" es mayor o igual que 30 cosa que se cumple
+
+
+
+//SEGUNDA EXPRESIÓN
+//let x = 15 
+//let y = 2 
+//!( x == "15" || x === y ) && ! (y !== 2 && x <= y)
+
+//!(X==15  || x===y) 
+//Aquí se devuelve true ya que al menos una de las dos condiciones se cumple (x==15)
+// !(y !== 2 && x <= y)
+//Aqui se devuelve false ya que las dos condiciones son falsas
+//Por lo tanto el resultado final seria false ya que una expresion devuelve true y la otra false y para devolver true las dos expresiones tienen que devolver true.
+
---
+++ Ejercicios tema1 _2.2/Ejercicio7/ejercicio7.js
@@ -0,0 +1,25 @@
+let string1 = "pepe";
+let string2 = "pepe";
+//1 Utiliza la consola para escribir una expresión que compare dos strings y produzca un valor TRUE
+let resultado1= string1===string2;
+console.log(resultado1);
+
+
+//2 Escribí una expresión comparando dos strings que devuelva FALSE.
+let var1 = "hola";
+let var2 = "adios";
+
+let resultado2 = var1===var2;
+console.log(resultado2);
+
+//3 Escribí una expresión que involucre un string y un número y devuelva FALSE
+let var3 = "trece";
+let var4 = 13;
+let resultado3 = var3===var4;
+console.log(resultado3);
+
+//Escribí una expresión que involucre un string y un número y devuelva TRUE
+let var5 = "15";
+let var6= 15;
+let resultado4 = var5==var6;
+console.log(resultado4);
